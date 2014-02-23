83e31c2eb5bcaf6106b547a1ecf0fc8585e70f37
Alexandre Masselot <masselot.alexandre@gene.com>
2014-02-23 14:23:16 -0800

    demo app with Future

--- build.sbt
+++ build.sbt
@@ -9,17 +9,19 @@ name := """reactive-pipo"""
 
 
 libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-actor" % "2.2.1",
-  "com.typesafe.akka" %% "akka-testkit" % "2.2.1",
+  "com.typesafe.akka" %% "akka-actor" % "2.2.3",
+  "com.typesafe.akka" %% "akka-testkit" % "2.2.3",
   "org.specs2" %% "specs2" % "2.3.4" % "test",
   "org.scalatest" % "scalatest_2.10" % "2.0" % "test",
-  "org.apache.commons" % "commons-math3" % "3.2"
+  "org.apache.commons" % "commons-math3" % "3.2",
+  "org.scala-lang.modules" %% "scala-async" % "0.9.0-M4"
 )
 
 // if you have more than one main method, you can specify which is used when typing 'run' in sbt
 //mainClass := Some("com.mycode.App")
 
-resolvers ++= Seq("snapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
+resolvers ++= Seq(
+  "snapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
   "releases" at "http://oss.sonatype.org/content/repositories/releases"
 )
 
--- src/main/scala/pipo/app/AppFuture.scala
+++ src/main/scala/pipo/app/AppFuture.scala
@@ -1,25 +1,33 @@
 package pipo.app
 
-import pipo.{Match, DataContainer, MatcherBasic}
-import scala.util.Try
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+import pipo._
+import scala.async.Async._
+import scala.util.Success
+import scala.util.Failure
 
 /**
  * Created by Alexandre Masselot on 2/21/14.
  */
-
 object AppFuture extends CommonsApp {
   output(s"start ${this.getClass.getSimpleName}")
 
   val data = DataContainer("one shot", 100, 1000000)
-  val matcher = new MatcherBasic(List(10, 50, 99, 25, 2, 3, 4, 5, 7, 8, 9), 3.0, data, 30)
+  val matcher = new MatcherWithTry(List(10, 50, 99, 25, 2, 3, 4, 5, 7, 8, 9), 3.0, data, 30)
 
-//  val matched = Async {
-//    matcher.findAll.toList
-//  }
-//
-//  for (m <- matched) {
-//    output(m.toString)
-//  }
+  val futureMatched = async {
+    matcher.findAll.toList
+  }
+  futureMatched onComplete {
+    case Success(matched) => for (m <- matched) {
+      output(m.toString)
+    }
+    case Failure(t) => println(t)
+  }
 
+  output("I'm free!")
+
+  Thread.sleep(10000)
   output("done")
 }
