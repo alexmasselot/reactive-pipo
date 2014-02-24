ea8304c7af6a5608c5c6a998de5d7a54e00c15ed
Alexandre Masselot <masselot.alexandre@gene.com>
2014-02-23 23:18:21 -0800

    just straight to iterator

---
+++ src/main/scala/pipo/MatcherWithTryIterator.scala
@@ -0,0 +1,53 @@
+package pipo
+
+import scala.util.{Try, Failure, Success}
+
+
+/**
+ *  Almost the same as EithTry, excpet that the artificial toList in findAll (to kill the iterator) is removed...
+ * Created by Alexandre Masselot on 2/23/14.
+
+ * @param targets the list of values that must be found
+ * @param minScore the minimum score to keep it
+ * @param source  the container of targeted values
+ * @param sliceWidth  the size of a slice
+ *
+ */
+class MatcherWithTryIterator(val targets: List[Int], val minScore: Double, val source: DataContainer, val sliceWidth: Int) extends MatcherCommon {
+  /**
+   * find the first occurrence
+   * @return an potential Match (failing Match are returned with a None)
+   */
+  def findOne: Option[Match] = {
+    buildSlices.find(isSliceMatching) match {
+      // if something was found, we build a Match object out of it
+      //      case Some(slice) => Some(buildMatchFromSlice(slice))
+      case Some(slice) =>
+        buildMatchFromSlice(slice) match {
+          case Success(m) => Some(m)
+          case Failure(e) => None //OK, failing silently is maybe not the best options
+        }
+      case None => None
+    }
+  }
+
+  /**
+   * get the list of all the matches
+   */
+  def findAll: Iterator[Try[Match]] = buildSlices.filter(isSliceMatching).map(buildMatchFromSlice)
+
+  /**
+   * from a matching slice, build a Match object from the given slice
+   * @param slice a slice of values from source
+   * @return
+   */
+  private def buildMatchFromSlice(slice: List[Int]): Try[Match] = {
+    val commonValues = slice.intersect(targets)
+    try {
+      Success(new Match(srcName = source.name, score = score(commonValues), matchedValues = commonValues.toList))
+    } catch {
+      case e: BadMatchException => Failure(e)
+    }
+  }
+
+}
