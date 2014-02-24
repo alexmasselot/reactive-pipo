ec1a335f9fb02cbf2f718de9850edccf10f79656
Alexandre Masselot <masselot.alexandre@gene.com>
2014-02-23 21:42:03 -0800

    error reports src name

--- src/main/scala/pipo/Match.scala
+++ src/main/scala/pipo/Match.scala
@@ -5,14 +5,14 @@ import scala.util.Random
 
 /**
  * an object created by a Matcher
- * THrough the companion object, one can set a failureRate, i.e. the probability to raise an BadMatchException at instance time
+ * Through the companion object, one can set a failureRate, i.e. the probability to raise an BadMatchException at instance time
  *
  * @param srcName typically the DummyData holder name
  * @param matchedValues the list of matched values
  */
 class Match(val srcName:String, val score:Double, val matchedValues:List[Int]) {
   if(Match.rndFail.nextFloat()<Match.failureRate){
-    throw new BadMatchException("could build it")
+    throw new BadMatchException(s"fake exception from matching $srcName")
   }
 
   override def toString =  f"source=$srcName, score=$score%2.2f, matched=$matchedValues"
